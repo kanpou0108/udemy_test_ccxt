b73d10878826c11c96bbe789477ed9a9fee4c5a9
Junpei Ihara <seapig.dolphin2925@gmail.com>
2018-11-18 19:38:29 +0900

    Add average-cryptowatch.js

---
+++ average-cryptowatch.js
@@ -0,0 +1,31 @@
+const request = require('request')
+const gauss = require('gauss');
+// https://cryptowatch.jp/docs/api#market-ohlc
+const ohlc1day = 86400
+const targetPeriod = 10
+
+const asyncFunc = () => {
+  return new Promise((resolve) => {
+    request(`https://api.cryptowat.ch/markets/bitflyer/btcjpy/ohlc?periods=${ohlc1day}`, (err, response, body) => {
+      resolve(JSON.parse(body))
+    })
+  })
+}
+
+async function main () {
+  const json = await asyncFunc()
+  let list = json.result[String(ohlc1day)]
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice#Parameters
+  // list = list.splice(list.length - 25, list.length)
+  list = list.splice(list.length - targetPeriod)
+  const closePriceList = list.map(entry => entry[4])
+  console.log('output', closePriceList)
+
+  // https://github.com/fredrick/gauss#instantiation
+  // https://github.com/fredrick/gauss#vectorsma
+  const prices = new gauss.Vector(closePriceList)
+  const vectorSma = prices.sma(closePriceList.length)
+  console.log(vectorSma);
+}
+
+main()
